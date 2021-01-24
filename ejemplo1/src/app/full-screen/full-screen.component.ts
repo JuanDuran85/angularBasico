d6b181f0eba25c4bf3b18dc3956e60e744417952
JuanDuran85 <duran85juan@gmail.com>
2021-01-24 19:35:44 -0300

    trabajando con componentes

---
+++ ejemplo1/src/app/full-screen/full-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-full-screen',
+  templateUrl: './full-screen.component.html',
+  styleUrls: ['./full-screen.component.sass']
+})
+export class FullScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
