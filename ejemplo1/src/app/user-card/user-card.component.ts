83910cbe345cb95b5102ef0de35a7dac2a46b93f
JuanDuran85 <duran85juan@gmail.com>
2021-01-31 13:22:33 -0300

    trabajando con eventos

--- ejemplo1/src/app/user-card/user-card.component.ts
+++ ejemplo1/src/app/user-card/user-card.component.ts
@@ -5,11 +5,29 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './user-card.component.html',
   styleUrls: ['./user-card.component.sass']
 })
+
 export class UserCardComponent implements OnInit {
+  public name : string;
+  public userName : string;
+  public avatar : string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() { 
+    this.name = "";
+    this.userName = "";
+    this.avatar = "";
   }
 
+  ngOnInit(): void {
+    this.name = "Juan";
+    this.userName = "JuanDuran85";
+    this.avatar = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/User_icon_2.svg/1200px-User_icon_2.svg.png";
+  }
+
+  changingUser(event : any){
+    console.log(event);
+    this.userName = event.target.value;
+  }
+  changingName(event : any){
+    this.name = event.target.value;
+  }
 }
