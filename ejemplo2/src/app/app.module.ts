7a14534463ced16073bb1502628c6144f8c0d9ea
JuanDuran85 <duran85juan@gmail.com>
2021-02-10 12:08:32 -0300

    trabajando con componentes

--- ejemplo2/src/app/app.module.ts
+++ ejemplo2/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ContadorComponent } from './contador/contador.component';
+import { HeroeComponent } from "./heroes/heroe/heroe.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContadorComponent
+    ContadorComponent,
+    HeroeComponent
   ],
   imports: [
     BrowserModule
