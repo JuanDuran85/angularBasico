42aad190255ea71560a1cb15cdd0decd383fed4b
JuanDuran85 <duran85juan@gmail.com>
2021-02-18 10:01:32 -0300

    trabajando con rutas

--- paisesApp/src/app/shared/shared.module.ts
+++ paisesApp/src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AppRoutingModule } from '../app-routing.module';
+import { RouterModule } from '@angular/router';
+
 import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
@@ -12,7 +13,7 @@ import { SidebarComponent } from './sidebar/sidebar.component';
   ],
   imports: [
     CommonModule,
-    AppRoutingModule
+    RouterModule
   ]
 })
 export class SharedModule { }
