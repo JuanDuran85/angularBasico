0c31e6aaa6e85bef0ced86d489be248f31d7b67a
JuanDuran85 <duran85juan@gmail.com>
2021-03-14 21:04:03 -0300

    trabajando con primeNG para UI

--- pipesApp/src/app/app.component.ts
+++ pipesApp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as faker from 'faker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pipesApp';
+  public nombre = faker.name.findName();
+
+  mostrarNombre(){
+    console.log(this.nombre);
+  }
 }
