7459ad87c3469232741ee90099046516a919f85e
JuanDuran85 <duran85juan@gmail.com>
2021-03-22 22:31:52 -0300

    trabajando con pipes de angular i18n

--- pipesApp/src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ pipesApp/src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -7,9 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoComunesComponent implements OnInit {
 
+  public nombre : string;
+  public genero : string;
+  public invitaMap : Object;
+  public clientes : string[];
+  public clientesMapa : Object;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.nombre = "Yecenia";
+    this.genero = "femenino";
+    this.invitaMap = {
+      'masculino' : 'invitarlo',
+      'femenino' : 'invitarla',
+    };
+    
+    this.clientes = ['Maria','Pedro','Jose','Yecenia'];
+    this.clientesMapa = {
+      '=0' : 'no tenemos clientes esperando',
+      '=1' : 'tenemos un cliente esperando',
+      '=2' : 'tenemos 2 clientes esperando',
+      'other' : 'tenemos # clientes esperando'
+    }
+  }
+
+  borrando(){
+    this.clientes.pop();
+  }
+
+  cambiarNombre(){
+    if (this.genero === "masculino") {
+      this.nombre = "Maria";
+      this.genero = "femenino";
+    } else {
+      this.nombre = "Juan";
+      this.genero = "masculino";
+    }
   }
 
 }
