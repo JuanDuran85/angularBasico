4f446904cdefdb2ea78179e8f7388b5b310cbb96
JuanDuran85 <duran85juan@gmail.com>
2021-03-27 23:16:35 -0300

    trabajando con nueva aplicacion con rutas lazy load - heroes

---
+++ heroesApp/src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { RouterModule } from '@angular/router';
+import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
+
+const routes : Routes = [
+  {
+    path: '404',
+    component: ErrorPageComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '404'
+  }
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+
+export class AppRoutingModule { }
