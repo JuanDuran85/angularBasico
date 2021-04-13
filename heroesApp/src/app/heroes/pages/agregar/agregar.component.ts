3260859ad2c860777b0017866b5fc928a76e9ae2
JuanDuran85 <duran85juan@gmail.com>
2021-04-12 22:44:10 -0400

    trabajando con formulario para agregar heroes

--- heroesApp/src/app/heroes/pages/agregar/agregar.component.ts
+++ heroesApp/src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Heroe, Publisher } from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-agregar',
@@ -9,6 +10,26 @@ import { ActivatedRoute } from '@angular/router';
 
 export class AgregarComponent implements OnInit {
   
+  public creators : any[] = [
+    {
+      id: "DC Comics",
+      desc: "DC Comics"
+    },
+    {
+      id: "Marvel Comics",
+      desc: "Marvel - Comics"
+    }
+  ];
+
+  public heroe : Heroe = {
+    superhero: '',
+    alter_ego: '',
+    first_appearance: '',
+    publisher: Publisher.DCComics,
+    alt_img: '',
+    characters: ''
+  }
+
   constructor(private activatedRoute : ActivatedRoute) { }
 
   ngOnInit(): void {
--- heroesApp/src/app/material/material.module.ts
+++ heroesApp/src/app/material/material.module.ts
@@ -7,7 +7,8 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input'; 
 import { MatListModule } from '@angular/material/list'; 
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'; 
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
@@ -24,6 +25,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatInputModule,
     MatListModule,
     MatProgressSpinnerModule,
+    MatSelectModule,
     MatSidenavModule,
     MatToolbarModule,
   ]
