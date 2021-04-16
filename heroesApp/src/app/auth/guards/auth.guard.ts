ac3d8c230381b5162ee677f909deecd3eacd93fe
JuanDuran85 <duran85juan@gmail.com>
2021-04-15 21:16:54 -0400

    verificando y validando ingreso de usuario con el guard de angular

--- heroesApp/src/app/auth/guards/auth.guard.ts
+++ heroesApp/src/app/auth/guards/auth.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
@@ -11,18 +12,37 @@ export class AuthGuard implements CanActivate, CanLoad {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-      if (this.authService.auth.id || localStorage.getItem('id')) {
+
+      return this.authService.validateAuth()
+        .pipe(
+          tap(resp => {
+            if (!resp) {
+              this.router.navigate(['./auth/login']);
+            }
+          })
+        );
+
+/*       if (this.authService.auth.id) {
         return true
       }
-      return false;
+      return false; */
   }
 
-  constructor(private authService : AuthService){}
+  constructor(private authService : AuthService, private router : Router){}
 
-  canLoad( route: Route, segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
-    if (this.authService.auth.id || localStorage.getItem('id')) {
+  canLoad( route: Route, segments: UrlSegment[]): Observable<boolean> | boolean {
+
+    return this.authService.validateAuth() // resulve un boolean o observable
+    .pipe(
+      tap(resp => {
+        if (!resp) {
+          this.router.navigate(['./auth/login']);
+        }
+      })
+    );
+/*     if (this.authService.auth.id || localStorage.getItem('id')) {
       return true
     }
-    return false;
+    return false; */
   }
 }
--- heroesApp/src/app/auth/services/auth.service.ts
+++ heroesApp/src/app/auth/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { tap } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Auth } from '../interfaces/auth.interface';
 
@@ -17,6 +18,20 @@ export class AuthService {
   }
 
   constructor(private http : HttpClient) { }
+
+  validateAuth() : Observable<boolean>{
+    if (!localStorage.getItem('id')) {
+      return of(false);
+    };
+
+    return this.http.get<Auth>(`${this._baseUrl}/usuarios/1`)
+    .pipe(
+      map(resp => {
+        this._auth = resp;
+        return true;
+      })
+    )
+  }
 
   login() {
     return this.http.get<Auth>(`${this._baseUrl}/usuarios/1`).pipe(
