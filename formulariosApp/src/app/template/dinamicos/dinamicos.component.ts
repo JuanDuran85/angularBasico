2e93c99a0a39cf27ffacbd465caf7dfad7ca42b4
JuanDuran85 <duran85juan@gmail.com>
2021-04-17 17:40:45 -0400

    trabajando con formularios template dinamicos

--- formulariosApp/src/app/template/dinamicos/dinamicos.component.ts
+++ formulariosApp/src/app/template/dinamicos/dinamicos.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Favorito, Persona } from '../interfaces/dinamicos.interface';
 
 @Component({
   selector: 'app-dinamicos',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DinamicosComponent implements OnInit {
 
+  @ViewChild('formularioDinamico') formularioDinamico! : NgForm;
+
+  public persona : Persona = {
+    nombre: "Juan",
+    favoritos: [
+      {id: 1, nombre: "Swat 4"},
+      {id: 2, nombre: "GTA San Andrea"},
+    ]
+  }
+
+  public newGame : string = "";
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  saveInfo():void{
+    console.log("guardado");
+  }
+
+  deleteGame(index : number):void{
+    this.persona.favoritos.splice(index,1);
+  }
+
+  addFavorito(): void{
+    const newFavorito : Favorito = {
+      id: this.persona.favoritos.length+1,
+      nombre: this.newGame
+    }
+    this.persona.favoritos.push({...newFavorito});
+    this.newGame = "";
   }
 
 }
