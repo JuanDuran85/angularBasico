2a113a9cfe87e2e2bf49fab176ef12b1b56f8321
JuanDuran85 <duran85juan@gmail.com>
2021-04-17 23:15:03 -0400

    trabajando con formularios ractivos

--- formulariosApp/src/app/reactive/basicos/basicos.component.ts
+++ formulariosApp/src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicosComponent implements OnInit {
 
-  constructor() { }
+ /*  public formularioBasico : FormGroup = new FormGroup({
+    nombre: new FormControl('Primer Valor'),
+    precio: new FormControl(0),
+    existencia: new FormControl(0),
+  }) */
+
+  formularioBasico : FormGroup = this.formBuilder.group({
+    nombre: ['', [Validators.required, Validators.minLength(3)]], //valor, validador sincrono, validador asincronos
+    precio: [0, [Validators.required, Validators.min(0)]],
+    existencia: [0,[Validators.required, Validators.min(0)]],
+  })
+
+  constructor(private formBuilder : FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  validNombre(): boolean{
+    return this.formularioBasico.controls.nombre.errors && this.formularioBasico.controls.nombre.touched;
   }
 
 }
