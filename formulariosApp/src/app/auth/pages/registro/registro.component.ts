0f8904933bd27ab0e64ab9fe009941756170cf68
JuanDuran85 <duran85juan@gmail.com>
2021-04-24 17:57:13 -0400

    trabajando con validarores para formularios reactivos

--- formulariosApp/src/app/auth/pages/registro/registro.component.ts
+++ formulariosApp/src/app/auth/pages/registro/registro.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.scss']
 })
+
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  //TODO: se va a mover a otra parte
+  public expreRegular : string = '([a-zA-Z]+) ([a-zA-Z]+)';
+
+  public miFormulario : FormGroup = this.formBuilder.group({
+    nombre: ['', [Validators.required, Validators.pattern(this.expreRegular)]]
+  })
+
+  constructor(private formBuilder : FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+  campoNoValido(campo: string) : any {
+    console.log(this.miFormulario.controls[campo].invalid);
+    return this.miFormulario.get(campo)?.invalid && this.miFormulario.get(campo)?.touched;
+  }
 }
