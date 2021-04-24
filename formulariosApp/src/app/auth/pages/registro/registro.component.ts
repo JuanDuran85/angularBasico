ea47e88f5b422a0b8ed35748ae72e387a4820f77
JuanDuran85 <duran85juan@gmail.com>
2021-04-24 18:54:43 -0400

    utilizando validaciones externas para formularios reactivos

--- formulariosApp/src/app/auth/pages/registro/registro.component.ts
+++ formulariosApp/src/app/auth/pages/registro/registro.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { emailRegular, expreRegular, validadorExterno } from 'src/app/shared/validator/validaciones';
 
 @Component({
   selector: 'app-registro',
@@ -9,20 +10,28 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 export class RegistroComponent implements OnInit {
 
-  //TODO: se va a mover a otra parte
-  public expreRegular : string = '([a-zA-Z]+) ([a-zA-Z]+)';
-
   public miFormulario : FormGroup = this.formBuilder.group({
-    nombre: ['', [Validators.required, Validators.pattern(this.expreRegular)]]
+    nombre: ['', [Validators.required, Validators.pattern(expreRegular)]],
+    email: ['',[Validators.required, Validators.email, Validators.pattern(emailRegular)]],
+    username: ['', [Validators.required, validadorExterno ]]
   })
 
   constructor(private formBuilder : FormBuilder) { }
 
   ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre: "AAAA BBBB",
+      email: 'user1@user1.com'
+    })
   }
 
-  campoNoValido(campo: string) : any {
-    console.log(this.miFormulario.controls[campo].invalid);
+  campoNoValido(campo: string) : boolean {
+    //console.log(this.miFormulario.controls[campo].invalid);
     return this.miFormulario.get(campo)?.invalid && this.miFormulario.get(campo)?.touched;
+  }
+
+  submitFormulario() : void {
+    //console.log(this.miFormulario.value);
+    this.miFormulario.markAllAsTouched();
   }
 }
---
+++ formulariosApp/src/app/shared/validator/validaciones.ts
@@ -0,0 +1,16 @@
+import { FormControl } from "@angular/forms";
+
+export const expreRegular : string = '([a-zA-Z]+) ([a-zA-Z]+)';
+export const emailRegular : string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+
+export const validadorExterno = (campo: FormControl) : object => {
+    const valor : string = campo.value?.trim().toLowerCase();
+    
+    if (valor === "juan"){
+      return {
+        validadorExt : true
+      }
+    };
+
+    return null;
+}
