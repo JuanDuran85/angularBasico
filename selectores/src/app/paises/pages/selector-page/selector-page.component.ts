5bc7f557602ba311686a927ae2a02235b536953f
JuanDuran85 <duran85juan@gmail.com>
2021-04-25 18:56:51 -0400

    selectorres anidados en formularios reactivos

--- selectores/src/app/paises/pages/selector-page/selector-page.component.ts
+++ selectores/src/app/paises/pages/selector-page/selector-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { switchMap, tap } from "rxjs/operators";
+import { PaisSmall } from '../../interfaces/paises.interface';
+import { PaisesService } from '../../services/paises.service';
 
 @Component({
   selector: 'app-selector-page',
@@ -7,9 +11,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelectorPageComponent implements OnInit {
 
-  constructor() { }
+  public miFormulario : FormGroup = this._fBuilder.group({
+    region: ['',[Validators.required]],
+    pais: ['',[Validators.required]],
+    frontera: ['',[Validators.required]]
+  })
+
+  //llenar selectores
+
+  public regiones : string[] = [];
+  public paisesRegion : PaisSmall[] = [];
+
+  constructor(private _fBuilder : FormBuilder, private paisService : PaisesService) { }
 
   ngOnInit(): void {
+    this.regiones = this.paisService.regiones;
+    
+    // cuando cambia el selector de regiones
+/*     this.miFormulario.get('region')?.valueChanges.subscribe(result => {
+      console.log(result)
+      this.paisService.getCountryRegion(result).subscribe(result => {
+        console.log(result)
+        this.paisesRegion = result;
+      })
+    }); */
+    
+    // otra manera de hacer el codigo de arriba
+    this.miFormulario.get('region')?.valueChanges
+    .pipe(
+      tap((_)=> this.miFormulario.get('pais')?.reset('')),
+      switchMap( region => this.paisService.getCountryRegion(region))
+      ).subscribe(paises => {
+        this.paisesRegion = paises;
+    })
   }
 
+  sevaData():void{
+    console.log(this.miFormulario.value);
+  }
 }
--- selectores/src/app/paises/services/paises.service.ts
+++ selectores/src/app/paises/services/paises.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { PaisSmall } from '../interfaces/paises.interface';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PaisesServiceService {
+export class PaisesService {
 
-  constructor() { }
+  private _regiones : string[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  private _baseUrl : string = "https://restcountries.eu/rest/v2";
+
+  get regiones() : string[]{
+    return [...this._regiones]
+  }
+
+  constructor(private _http : HttpClient) { }
+
+  getCountryRegion(region : string) : Observable<PaisSmall[]>{
+    const url : string = `${this._baseUrl}/region/${region}?fields=name;alpha3Code`;
+    return this._http.get<PaisSmall[]>(url);
+  }
 }
