333504f8339dd209f2a085e60faebd46eb7e734f
JuanDuran85 <duran85juan@gmail.com>
2021-04-25 02:16:38 -0400

    validaciones asincronas en formaularios reactivos

--- formulariosApp/src/app/auth/pages/registro/registro.component.ts
+++ formulariosApp/src/app/auth/pages/registro/registro.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { EmailValidatorService } from 'src/app/shared/validator/email-validator.service';
 /* import { emailRegular, expreRegular, validadorExterno } from 'src/app/shared/validator/validaciones'; */
 import { ValidatorService } from 'src/app/shared/validator/validator.service';
 
@@ -13,7 +14,7 @@ export class RegistroComponent implements OnInit {
 
   public miFormulario : FormGroup = this.formBuilder.group({
     nombre: ['', [Validators.required, Validators.pattern(this.vs.expreRegular)]],
-    email: ['',[Validators.required, Validators.email, Validators.pattern(this.vs.emailRegular)]],
+    email: ['',[Validators.required, Validators.email, Validators.pattern(this.vs.emailRegular)], [this.emailValidate]],
     username: ['', [Validators.required, this.vs.validadorExterno ]],
     password: ['',[Validators.required, Validators.minLength(6)]],
     password2: ['',[Validators.required]],
@@ -21,7 +22,21 @@ export class RegistroComponent implements OnInit {
     validators: [this.vs.sonIguales('password', 'password2')]
   })
 
-  constructor(private formBuilder : FormBuilder, private vs: ValidatorService) { }
+  get emailErrors() : string {
+    
+    const errorsEmail : ValidationErrors = this.miFormulario.get('email')?.errors;
+    if (errorsEmail?.required) {
+      return "El correo es obligatorio"
+    } else if (errorsEmail?.pattern) {
+      return "El correo no tiene formato adecuado"
+    } else if (errorsEmail?.ocupado) {
+      return "El correo ya se encuentra en uso"
+    } else {
+      return "";
+    }
+  }
+
+  constructor(private formBuilder : FormBuilder, private vs: ValidatorService, private emailValidate : EmailValidatorService) { }
 
   ngOnInit(): void {
     this.miFormulario.reset({
---
+++ formulariosApp/src/app/shared/validator/email-validator.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AbstractControl, AsyncValidator, ValidationErrors } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, delay } from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmailValidatorService implements AsyncValidator {
+
+  constructor(private http : HttpClient) { }
+
+  validate(control: AbstractControl): Observable<ValidationErrors | null> {
+    console.log(control.value);
+
+    return this.http.get<any[]>(`http://localhost:3000/usuarios?q=${control.value}`).pipe(
+      delay(4500),
+      map((resultado:any[]) => {
+        return resultado.length === 0 ? null : {ocupado:true};
+      }))
+  }
+}
