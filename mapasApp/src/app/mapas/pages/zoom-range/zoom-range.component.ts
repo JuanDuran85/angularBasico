47168c2edfca96df1e010151e5cfc51d7e356f65
JuanDuran85 <duran85juan@gmail.com>
2021-05-01 19:44:32 -0400

    trabajando con el zoom en los mapas

--- mapasApp/src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ mapasApp/src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl'
 
 @Component({
@@ -6,33 +6,61 @@ import * as mapboxgl from 'mapbox-gl'
   templateUrl: './zoom-range.component.html',
   styleUrls: ['./zoom-range.component.scss']
 })
-export class ZoomRangeComponent implements OnInit, AfterViewInit {
+export class ZoomRangeComponent implements OnInit, AfterViewInit, OnDestroy {
 
-  public mapa! : mapboxgl.Map;
+  public mapaFinal! : mapboxgl.Map;
+  public zoomLavel : number = 10;
+  public centro : [number,number] = [-70.61200807074893,-33.43786874276012]
 
   @ViewChild('mapa') divMapa! : ElementRef;
 
   constructor() { }
+  
+  ngOnDestroy(): void {
+    this.mapaFinal.off('zoom',()=>{});
+    this.mapaFinal.off('zoomend',()=>{});
+    this.mapaFinal.off('move',()=>{});
+  }
 
   ngAfterViewInit(): void {
-    this.mapa = new mapboxgl.Map({
+    this.mapaFinal = new mapboxgl.Map({
       container: this.divMapa.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-70.61200807074893,-33.43786874276012],
-      zoom: 18
+      center: this.centro,
+      zoom: this.zoomLavel
     });
+
+    this.mapaFinal.on('zoom',(event)=>{
+      this.zoomLavel = this.mapaFinal.getZoom();
+    });
+
+    this.mapaFinal.on('zoomend',(event)=>{
+      if (this.mapaFinal.getZoom() > 18) {
+        this.mapaFinal.zoomTo(18);
+      }
+    });
+
+    this.mapaFinal.on('move',(event)=>{
+      const target = event.target;
+      const { lng , lat} = target.getCenter();
+      this.centro = [lng, lat];
+    })
   }
 
   ngOnInit(): void {
   }
 
   zoomIn() : void {
-    console.log("Zoom In");
-    this.mapa.zoomIn();
+    this.mapaFinal.zoomIn();
+    //this.zoomLavel = this.mapaFinal.getZoom();
+  }
+  
+  zoomOut() : void {
+    this.mapaFinal.zoomOut();
+    //this.zoomLavel = this.mapaFinal.getZoom();
   }
 
-  zoomOut() : void {
-    console.log("Zoom Out");
-    this.mapa.zoomOut();
+  zoomChange(value : string) : void{
+    this.mapaFinal.zoomTo(Number(value));
   }
 }
