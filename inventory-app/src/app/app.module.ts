f7bb62f206ef6f94282f0976ee1bac1b2d97e152
JuanDuran85 <duran85juan@gmail.com>
2025-01-30 16:51:50 -0300

    actualizar configuración de HttpClient y mejorar el diseño de la lista de productos

--- inventory-app/src/app/app.module.ts
+++ inventory-app/src/app/app.module.ts
@@ -4,12 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
-import { HttpClient } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, ProductListComponent],
   imports: [BrowserModule, AppRoutingModule],
-  providers: [HttpClient],
+  providers: [provideHttpClient()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
