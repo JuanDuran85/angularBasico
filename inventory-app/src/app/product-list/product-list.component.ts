53a6c01cfe01009cc6ecd60c743557214761c693
JuanDuran85 <duran85juan@gmail.com>
2025-01-30 16:15:07 -0300

    agregar componente de lista de productos y eliminar pruebas del componente principal

---
+++ inventory-app/src/app/product-list/product-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  standalone: false,
+
+  templateUrl: './product-list.component.html',
+})
+export class ProductListComponent {}
