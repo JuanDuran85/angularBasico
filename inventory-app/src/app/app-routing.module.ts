4039f319298dbedf364931c7ce130c6e6b362a2a
JuanDuran85 <duran85juan@gmail.com>
2025-01-30 17:34:45 -0300

    agregar componente para añadir productos y actualizar rutas de navegación

--- inventory-app/src/app/app-routing.module.ts
+++ inventory-app/src/app/app-routing.module.ts
@@ -1,21 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddProductComponent } from './add-product/add-product.component';
 import { ProductListComponent } from './product-list/product-list.component';
 
 const routes: Routes = [
   {
     path: 'products',
-    component: ProductListComponent
+    component: ProductListComponent,
   },
   {
     path: '',
     redirectTo: 'products',
-    pathMatch: 'full'
-  }
+    pathMatch: 'full',
+  },
+  {
+    path: 'add-product',
+    component: AddProductComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- inventory-app/src/app/app.module.ts
+++ inventory-app/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { provideHttpClient } from '@angular/common/http';
+import { AddProductComponent } from './add-product/add-product.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent],
+  declarations: [AppComponent, ProductListComponent, AddProductComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [provideHttpClient()],
   bootstrap: [AppComponent],
